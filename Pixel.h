3f4c925e7b09eb572a28355b2bb937fbaed56952
RobertFontan <87390691+RobertFontan@users.noreply.github.com>
2022-08-21 19:38:36 -0400

    Add files via upload

---
+++ Pixel.h
@@ -0,0 +1,19 @@
+#pragma once
+struct Pixel
+{
+	unsigned char blue;
+	unsigned char green;
+	unsigned char red;
+	
+	
+	Pixel();
+	Pixel(unsigned char blue, unsigned char green, unsigned char red);
+
+	// operators
+
+	//bool operator == (const Pixel& rhs) const;
+
+	//Pixel operator * (Pixel& rhs);
+
+};
+
